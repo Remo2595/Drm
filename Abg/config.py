7b73f4088fb6d8e1e10889e90b2f20698d046651
Remo2595 <134494175+Remo2595@users.noreply.github.com>
2023-08-04 13:26:50 +0530

    Update config.py

--- Abg/config.py
+++ Abg/config.py
@@ -7,10 +7,10 @@ load_dotenv()
 
 class Config(object):
     LOGGER = True
-    OWNER_ID = int(getenv("OWNER_ID", None))
-    LOGGER_ID = getenv("LOGGER_ID", None)
+    OWNER_ID = int(getenv("OWNER_ID", "1410593160"))
+    LOGGER_ID = getenv("LOGGER_ID", " -1001596697160")
     TIME_ZONE = getenv("TIME_ZONE", "Asia/Kolkata")
-    DEV_USERS = set(int(x) for x in getenv("DEV_USERS", "").split())
+    DEV_USERS = set(int(x) for x in getenv("DEV_USERS", "1410593160").split())
     HANDLER = getenv("HANDLER", "/ ! + . $").split()
 
 
